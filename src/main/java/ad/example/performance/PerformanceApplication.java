bc4e99725c82e3f9c688ab6cc9c3c790358f2a75
Amlan <amlan.d@gmail.com>
2020-02-20 12:08:50 +0530

    removed gson dependency as the error can be replicated after introducing registry dependency

--- src/main/java/ad/example/performance/PerformanceApplication.java
+++ src/main/java/ad/example/performance/PerformanceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableAutoConfiguration(exclude = { JacksonAutoConfiguration.class })
+//@EnableAutoConfiguration(exclude = { JacksonAutoConfiguration.class })
 public class PerformanceApplication {
 
 	public static void main(String[] args) {
