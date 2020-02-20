4dc0443a16d388e8073fc9ada6f5e30a88e49a60
Amlan <amlan.d@gmail.com>
2020-02-20 10:06:13 +0530

    cleaned up app security

--- src/main/java/ad/example/performance/ApplicationSecurity.java
+++ src/main/java/ad/example/performance/ApplicationSecurity.java
@@ -1,33 +1,19 @@
 package ad.example.performance;
 
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.RequestMatcher;
 
-@EnableWebSecurity
+@Configuration 
 public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .requestMatchers(appRequestMatcher()).permitAll()
-                .and().authorizeRequests()
-                .requestMatchers((request) -> request.getServletPath().equals("/")).permitAll()
-                .and().authorizeRequests()
-                .requestMatchers(endpointRequestMatcher()).permitAll()
-                .and().authorizeRequests()
-                .anyRequest().denyAll();
-    }
 
-    private RequestMatcher endpointRequestMatcher() {
-        return EndpointRequest.to("metrics", "prometheus");
-    }
-    
-
-    private RequestMatcher appRequestMatcher() {
-        return (request) -> request.getServletPath().startsWith("/actuator") ||
-        		request.getServletPath().startsWith("/simulate") ||
-        		request.getServletPath().startsWith("/health") ;
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests().anyRequest().permitAll()
+				.and()
+				.httpBasic().disable()
+				.csrf().disable();
+	}
 }
