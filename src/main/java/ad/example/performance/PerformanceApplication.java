478c4666e72ccdc32b05c09ae57d58eaffadd653
Amlan <amlan.d@gmail.com>
2020-02-20 11:38:46 +0530

    Included spring cloud registry and config server dependencies

--- src/main/java/ad/example/performance/PerformanceApplication.java
+++ src/main/java/ad/example/performance/PerformanceApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableAutoConfiguration(exclude = { JacksonAutoConfiguration.class })
 public class PerformanceApplication {
 
