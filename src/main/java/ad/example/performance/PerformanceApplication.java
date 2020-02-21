d03c5ba7709d22182d8b6524e88a7337b3b01b16
Amlan <amlan.d@gmail.com>
2020-02-21 10:03:56 +0530

    restoring back

--- src/main/java/ad/example/performance/PerformanceApplication.java
+++ src/main/java/ad/example/performance/PerformanceApplication.java
@@ -1,14 +1,11 @@
 package ad.example.performance;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableAutoConfiguration(exclude = { JacksonAutoConfiguration.class })
 public class PerformanceApplication {
 
 	public static void main(String[] args) {
